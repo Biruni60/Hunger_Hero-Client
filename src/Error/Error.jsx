5b1350ce6132a12420050b53a89a7842d83cddd9
biru60 <u1904060@student.cuet.ac.bd>
2023-11-05 20:38:50 +0600

    Footer Added

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div className="text-center">
+        <div className="text-center max-w-screen-xl mx-auto">
            <Link to='/'><button className="btn btn-outline text-sky-800 my-4">RETURN HOME</button></Link>
-            <img className="h-[90vh]" src="https://i.ibb.co/Hd4YXXV/3828537-1.jpg"/>
+            <img className="h-[90vh]  w-full" src="https://i.ibb.co/Hd4YXXV/3828537-1.jpg"/>
         </div>
     );
 };
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,9 +1,35 @@
+import { Link } from "react-router-dom";
 
 
 const Footer = () => {
     return (
         <div>
-            
+            <footer className="footer p-10 bg-base-200 text-base-content">
+  <aside>
+  <Link to="/" className="text-red-600  text-lg md:text-3xl font-semibold">HUNGER HEROS</Link>
+    <p>Food For All</p>
+  </aside> 
+  <nav>
+    <header className="footer-title">Services</header> 
+    <a className="link link-hover">Foods</a> 
+    <a className="link link-hover">Help</a> 
+    <a className="link link-hover">Donation</a> 
+    <a className="link link-hover">Kindness</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Company</header> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Legal</header> 
+    <a className="link link-hover">Terms of use</a> 
+    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
         </div>
     );
 };
