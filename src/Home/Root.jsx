c748147ef3f6b33eeb2018fba3e0bf4dc6bcc097
biru60 <u1904060@student.cuet.ac.bd>
2023-11-05 16:10:50 +0600

    signup  page created and user can signup

--- src/Home/Root.jsx
+++ src/Home/Root.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../NavComponent/Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 
 const Root = () => {
     return (
-        <div>
-            
+        <div className=" max-w-screen-xl mx-auto">
+           <Navbar></Navbar> 
+           <Outlet></Outlet>
+           <Footer></Footer>
         </div>
     );
 };
--- src/UserAuth/AuthProvider.jsx
+++ src/UserAuth/AuthProvider.jsx
@@ -1,11 +1,51 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types"
+import auth from "./firebase.init"
+import {  GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 
 
-const AuthProvider = () => {
+         export const AuthContext=createContext()
+
+const AuthProvider = ({children}) => {
+    const [user,SetUser]=useState(null);
+    const[loading,setLoading]=useState(true);
+   const provider=new GoogleAuthProvider();
+   const googleSignIn=()=>{
+    setLoading(true)
+    return signInWithPopup( auth ,provider)
+    
+   }
+  
+   const createUser=(email,password)=>{
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth,email,password)
+   }
+   const logIn=(email,password)=>{
+    setLoading(true)
+    return signInWithEmailAndPassword(auth,email,password)
+   }
+   const logOut=()=>{
+    return signOut(auth)
+   }
+   useEffect(()=>{
+    const unSubscribe=onAuthStateChanged(auth,currentUser=>{
+        SetUser(currentUser);
+        setLoading(false)
+    })
+    return ()=>{
+        return unSubscribe();
+    }
+   },[])
+    const authInfo={
+        user,loading,googleSignIn,createUser,logIn,logOut,
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
-
+AuthProvider.propTypes={
+    children:PropTypes.node
+}
 export default AuthProvider;
